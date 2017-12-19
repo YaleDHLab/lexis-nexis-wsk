268e35d12ab24de43fa8d37b8808d97a389d8e9e
duhaime <douglas.duhaime@gmail.com>
2017-12-19 16:19:17 -0500

    simplify module imports

--- setup.py
+++ setup.py
@@ -2,6 +2,11 @@ from setuptools import setup
 
 setup (
   name='wsk',
-  version='0.0.1',
-  scripts=['wsk.py']
+  version='0.1.0',
+  packages=['wsk'],
+  description="A light wrapper for LexisNexis's Web Services Kit API",
+  url='https://github.com/YaleDHLab/lexis-nexis-wsk',
+  author='Douglas Duhaime',
+  author_email='douglas.duhaime@gmail.com',
+  license='MIT'
 )
