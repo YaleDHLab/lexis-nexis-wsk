df23d5a61dd0ba61fb3eafd310ccb1289d9b6043
duhaime <douglas.duhaime@gmail.com>
2018-02-01 10:51:31 -0500

    update sample query

--- examples/example.py
+++ examples/example.py
@@ -20,8 +20,8 @@ sources = session.search_sources(query='times')
 # get the included and excluded publication titles for a source id
 source_details = session.get_source_details(source_id=161887)
 
-# run a query for quaternion within a given source id
-results = session.search(query='quaternion', source_id=161887,
+# run a query for a keyword within a given source id
+results = session.search(query='peppers', source_id=161887,
     start_date='2017-10-01', end_date='2017-10-24',
     save_results=True, return_results=True)
 
